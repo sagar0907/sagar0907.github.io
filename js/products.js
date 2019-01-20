c03acd2113ebb122b1cecaff159c227210af6b73
Sagar Jain <sagarjain0907@gmail.com>
2019-01-20 19:25:12 +0530

    ui changes

--- js/products.js
+++ js/products.js
@@ -1,7 +1,33 @@
-$(document).ready(function () {
+function initClickHandlers() {
     $('.down-arrow').click(function () {
         $([document.documentElement, document.body]).animate({
             scrollTop: $(".display-slab").offset().top
         }, 1000);
     });
+}
+
+function initScrollHandlers() {
+    let slabs = $(".display-slab");
+    $(window).scroll(function () {
+        let scrollTop = $(window).scrollTop();
+        for (let i = 0; i < slabs.length; i++) {
+            let slab = $(slabs[i]);
+            if (scrollTop > slab.offset().top - $(window).height()) {
+                let leftPart = slab.find(".box-part-left");
+                let rightPart = slab.find(".box-part-right");
+                leftPart.css("right", "0");
+                rightPart.css("left", "0");
+                setTimeout(function() {
+                    leftPart.removeClass("box-part-left");
+                    rightPart.removeClass("box-part-right");
+                },2000);
+            }
+        }
+    });
+}
+
+
+$(document).ready(function () {
+    initClickHandlers();
+    initScrollHandlers()
 });
